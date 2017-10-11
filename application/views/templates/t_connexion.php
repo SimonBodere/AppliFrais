dbf146ab639718694bff06fd5a628774fa6efc0e
SimonBodere <simonpierre.bodere@gmail.com>
2017-10-11 13:44:31 +0200

    yo

--- application/views/templates/t_connexion.php
+++ application/views/templates/t_connexion.php
@@ -1,34 +1,43 @@
 ﻿<?php
-	$this->load->helper('url');
-	$v_path = base_url('application/views');
+$this->load->helper('url');
+$v_path = base_url('application/views');
 ?>
-
+ 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
-
-	<head>
-		<title>Intranet du Laboratoire Galaxy-Swiss Bourdin</title>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<link href="<?php echo $v_path.'/templates/css/styles.css'?>" rel="stylesheet" type="text/css" />
-	</head>
-
-	<body>
-		<div id="page">
-			<div id="entete">
-				<img src="<?php echo $v_path.'/templates/images/logo.jpg'?>" id="logoGSB" alt="Laboratoire Galaxy-Swiss Bourdin" title="Laboratoire Galaxy-Swiss Bourdin" />
-				<h1>Suivi du remboursement des frais</h1>
-			</div>
-
-			<?php echo $body; ?>
-
-			<div id="pied">
-				<br/>
-			</div>
-
-		</div>    
-
-	</body>
+ 
+    <head>
+        <title>Intranet du Laboratoire Galaxy-Swiss Bourdin</title>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <link href="<?php echo $v_path.'/templates/css/styles.css'?>" rel="stylesheet" type="text/css" />
+    </head>
+ 
+    <body>
+        <div id="page">
+            <div id="entete">
+                <img src="<?php echo $v_path.'/templates/images/logo.jpg'?>" id="logoGSB" alt="Laboratoire Galaxy-Swiss Bourdin" title="Laboratoire Galaxy-Swiss Bourdin" />
+                <h1>Suivi du remboursement des frais</h1>
+            </div>
+ 
+            <?php echo $body; ?>
+ 
+            <div id="pied">
+                <p>
+                    <a href="http://validator.w3.org/check?uri=referer"><img
+                    src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a>
+                </p>
+               
+                <p>
+                    <a href="http://jigsaw.w3.org/css-validator/check/referer">
+                    <img style="border:0;width:88px;height:31px"
+                            src="http://jigsaw.w3.org/css-validator/images/vcss"
+                            alt="CSS Valide !" />
+                    </a>
+                </p>
+            </div>
+ 
+        </div>    
+ 
+    </body>
 </html>
-
-	  
--- application/views/v_visAccueil.php
+++ application/views/v_visAccueil.php
@@ -1,22 +1,18 @@
 <div id="contenu">
-	<h2>Gestion des frais des visiteurs</h2>
-	<p>Bienvenue dans votre application de gestion des frais de déplacements. </p>
-	<p>
-		Afin de couvrir les frais que vous avez engagés lors de vos déplacements professionnels, vous devez compléter des fiches de frais mensuelles.
-		<ul>
-			<li>Une fiche de frais court du 1er au dernier jour du mois;
-			<li>Les fiches de frais sont créées automatiquement par l'application au fil de votre utilisation;
-			<li>Vous complétez les fiches de frais à votre rythme;
-			<li>Lorsqu'une fiche est totalement renseignée, vous devrez la "signer" afin qu'elle puisse être traitée par le service comptable;
-		</ul>
-	</p>
-
-	<p>
-		Au moyen du bandeau gauche, vous avez accès aux fonctionalités 
-		du profil visiteur : 
-		<ul>
-			<li>Gérer vos fiches de frais (modifier, signer, imprimer)</li>
-			<li>Se déconnecter</li>
-		</ul>
-	</p>
+    <h2>Gestion des frais des visiteurs</h2>
+    <p>Bienvenue dans votre application de gestion des frais de deplacements. </p>
+    <p>Afin de couvrir les frais que vous avez engages lors de vos deplacements professionnels, vous devez completer des fiches de frais mensuelles.</p>
+        <ul>
+            <li>Une fiche de frais court du 1er au dernier jour du mois</li>
+            <li>Les fiches de frais sont creees automatiquement par l'application au fil de votre utilisation</li>
+            <li>Vous completez les fiches de frais a votre rythme</li>
+            <li>Lorsqu'une fiche est totalement renseignee, vous devrez la "signer" afin qu'elle puisse etre traitee par le service comptable</li>
+        </ul>
+ 
+        <p>Au moyen du bandeau gauche, vous avez acces aux fonctionalites du profil visiteur : </p>
+        <ul>
+            <li>Gerer vos fiches de frais (modifier, signer, imprimer)</li>
+            <li>Se deconnecter</li>
+        </ul>
+   
 </div>
